42574433745444360ca58ce019fe234061dff74d
Renato Golin <rengolin@systemcall.eu>
2024-11-22 18:46:46 +0000

    Add run all benchmark script + use internal libxsmm (#27)

--- prepare.sh
+++ prepare.sh
@@ -4,41 +4,43 @@
 HERE=$PWD
 
 echo "===================================== Configuring Checkout"
-git checkout dev-cpu-v2
 git submodule init
 git submodule update
 
 echo "===================================== Setting Up Conda Env"
-cd ./../
-export CONDA_INSTALL_DIR=`pwd`/miniforge
+export CONDA_INSTALL_DIR=$HERE/../miniforge
 export ENV_NAME=triton
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$(uname -m).sh"
-bash ./Miniforge3-Linux-$(uname -m).sh -b -p ${CONDA_INSTALL_DIR}
-${CONDA_INSTALL_DIR}/bin/conda create -y -n ${ENV_NAME} python=3.9
-source ${CONDA_INSTALL_DIR}/bin/activate ${ENV_NAME}
+if [ ! -d $CONDA_INSTALL_DIR ]; then
+  pushd ./../
+  wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$(uname -m).sh"
+  bash ./Miniforge3-Linux-$(uname -m).sh -b -p ${CONDA_INSTALL_DIR}
+  ${CONDA_INSTALL_DIR}/bin/conda create -y -n ${ENV_NAME} python=3.9
+  source ${CONDA_INSTALL_DIR}/bin/activate ${ENV_NAME}
 
-echo "===================================== Install Dependencies"
-pip install ninja cmake wheel pybind11 scipy numpy torch pytest lit pandas matplotlib
-if [ $? != 0 ]; then
-  exit 1
+  echo "===================================== Install Dependencies"
+  pip install ninja cmake wheel pybind11 scipy numpy torch pytest lit pandas matplotlib
+  if [ $? != 0 ]; then
+    exit 1
+  fi
+
+  popd  
+  conda deactivate
+else
+  echo "Miniconda already installed, skipping."
 fi
 
 echo "===================================== Building LLVM"
-git clone https://github.com/llvm/llvm-project.git
-cd llvm-project
-git checkout `cat ${HERE}/cmake/llvm-hash.txt`
-mkdir build
-cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=True -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_PROJECTS="mlir;llvm" -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU" ../llvm
-ninja
-cd ./../../
-
-echo "===================================== Building libxsmm"
-git clone https://github.com/libxsmm/libxsmm
-cd libxsmm
-make realclean && make CC=clang CXX=clang++ FC= STATIC=0 -j
-cd ./../
-
-cd $HERE
-
-conda deactivate
+if [ ! -d $HERE/../llvm-project ]; then
+  pushd ./../
+  git clone https://github.com/llvm/llvm-project.git
+  cd llvm-project
+  git checkout `cat ${HERE}/cmake/llvm-hash.txt`
+  mkdir -p build
+  pushd build
+  cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=True -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld -DLLVM_ENABLE_PROJECTS="mlir;llvm" -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU" ../llvm
+  ninja
+  popd
+  popd
+else
+  echo "LLVM already built, skipping."
+fi
