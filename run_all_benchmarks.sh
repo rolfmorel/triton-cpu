4ac87c32cae410b82b8f3e486aaf7339a21811c4
Rolf Morel <rolf.morel@intel.com>
2024-11-25 09:27:05 -0800

    Store commit hash with benchmark run details

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -6,18 +6,20 @@ source ../miniforge/bin/activate triton
 
 THREADS=$(lscpu | grep --color=never "Core.*socket" | grep -o "[0-9]\+")
 
+commit=$(git describe --always --dirty)
+
 for datatype in f32 bf16 bf8; do
   for num_threads in 1 $THREADS; do
     for backend in torch-cpu-compile torch-cpu-native; do
       config=baseline
-      echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype'}"
+      echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype', 'commit': '$commit'}"
       time $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $config $num_threads --datatype $datatype --backend $backend 2>&1
       echo -e "\n\n"
     done
 
     backend=triton-cpu
     for config in baseline-scalar baseline-block; do
-      echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype'}"
+      echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype', 'commit': '$commit'}"
       time $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $config $num_threads --datatype $datatype --backend $backend 2>&1
       echo -e "\n\n"
     done
@@ -25,13 +27,13 @@ for datatype in f32 bf16 bf8; do
     # Triton-XSMM
     backend=triton-xsmm
     for config in xsmm-scalar xsmm-block; do
-      echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype'}"
+      echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype', 'commit': '$commit'}"
       time $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $config $num_threads --datatype $datatype --backend $backend 2>&1
       echo -e "\n\n"
     done
     for config in xsmm-pad-k xsmm-loop-collapse-pad-b; do
       for external_pad in "" "--external-pad"; do
-        echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype', 'pad': '$external_pad'}"
+        echo -e "BENCHMARK: {'backend': '$backend', 'config': '$config', 'threads': $num_threads, 'type': '$datatype', 'pad': '$external_pad', 'commit': '$commit'}"
         time $SCRIPT_DIR/python/tutorials/03-matrix-multiplication-cpu.sh $config $num_threads --datatype $datatype --backend $backend $external_pad 2>&1
         echo -e "\n\n"
       done
