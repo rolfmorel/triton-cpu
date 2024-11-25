92d4251e23c0293beccf23b539bcae844511751f
Rolf Morel <rolf.morel@intel.com>
2024-11-25 07:13:41 -0800

    Improve raw-to-csv a la @rengoline and prepare for summary line to be parsed

--- raw-to-csv.py
+++ raw-to-csv.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python
 
-import io
 import sys
 import re
-from pprint import pprint
 
 def convert_all(in_file):
   metadata = None
   csv_lines = []
-  count_new_line_sep = 0
+
   for line in in_file:
-    if line.startswith('sys '):
-      if metadata:
-          yield (csv_lines, metadata)
-          metadata = None
-          csv_lines = []
-    if line in {"", "\n"}:
-      count_new_line_sep += 1
-      if count_new_line_sep >= 2:
-        if metadata:
-            yield (csv_lines, metadata)
-            metadata = None
-            csv_lines = []
-            count_new_line_sep = 0
     if line.startswith("RUN") or line.startswith("BENCHMARK"):
+      # If this is not the first RUN line, yield the previous bundle
+      if metadata and csv_lines:
+        yield (csv_lines, metadata)
+        metadata = None
+        csv_lines = []
       metadata = {'line': line}
     if m := re.match(r" +M.*", line): # header row
       csv_line = re.subn("[, ][, ]+", ",", line)
-      csv_lines += [csv_line[0][1:]]
+      l = csv_line[0][1:].split(",")
+      k_onwards = l[2:]
+      csv_lines += [",".join(k_onwards)]
     if m := re.match(r"\d+ +(\d.*)", line): # data row
       csv_line = re.subn("[, ]+", ",", m[1])
-      csv_lines += [csv_line[0]+"\n"]
+      l = csv_line[0][1:].split(",")
+      k_onwards = l[2:]
+      csv_lines += [",".join(k_onwards)+"\n"]
+
+  # The last bundle gets yielded here
+  if metadata and csv_lines:
+    yield (csv_lines, metadata)
 
 
 if __name__ == "__main__":
@@ -41,6 +39,4 @@ if __name__ == "__main__":
   for csv_lines, metadata in converted:
     print(metadata['line'], end='')
     for csv_line in csv_lines:
-      print(csv_line, end='') # csv_line includes its own newline
-
-
+      print(csv_line, end='')
